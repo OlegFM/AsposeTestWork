3535dca8eb8def04170841e93e363cb4c84f26b4
Oleg Fominsky <5587456+OlegFM@users.noreply.github.com>
2022-04-23 21:02:00 +0300

    Готовы скелеты 2/3
    Консольное приложение: практически готово, нужно будет только прикрутить перевод вместо обработки документа.
    
    Библиотека: Ещё нет перевода, процессинг документа в разработке.

---
+++ AsposeTestWork.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsposeTestWork.Core;
+using System.Globalization;
+
+string filePath = args[0];
+string lang = args[1];
+CultureInfo culture;
+CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+
+CheckPathToFile();
+CheckLang();
+
+WordProcessor processor = new WordProcessor();
+processor.ReadWordFile(filePath);
+processor.SetTranslationLang(culture);
+Console.WriteLine(processor.AsposeTestProcessing());
+
+
+void CheckPathToFile()
+{
+    if (filePath is null)
+    {
+        Console.WriteLine("Please, enter the path to Word file");
+        filePath = Console.ReadLine();
+    }
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine("File not exists.");
+        filePath = null;
+        CheckPathToFile();
+    }
+}
+
+void CheckLang()
+{
+    if (lang is null)
+    {
+        Console.WriteLine("Please, enter the language in format **-**");
+        lang = Console.ReadLine();      
+    }
+    culture = cultures.FirstOrDefault(c => c.Name == lang);
+    if (culture is null)
+    {
+        Console.WriteLine("Wrong, usupported culture.");
+        CheckLang();
+    }
+}
--- AsposeTestWork.Core/WordProcessor.cs
+++ AsposeTestWork.Core/WordProcessor.cs
@@ -1,7 +1,35 @@
-﻿namespace AsposeTestWork.Core
+﻿using Aspose.Words;
+using System.Globalization;
+using System.Text;
+
+namespace AsposeTestWork.Core
 {
     public class WordProcessor
     {
+        private Document _file;
+        private DocumentBuilder _docBuilder;
+        private CultureInfo _translationLang;
+        public void ReadWordFile(string path)
+        {
+            _file = new Document(path);
+            _docBuilder = new DocumentBuilder();
+        }
 
+        public void SetTranslationLang(CultureInfo culture)
+        {
+            _translationLang = culture;
+        }
+
+        public void Translate()
+        {
+
+        }
+        public string AsposeTestProcessing()
+        {
+            StringBuilder sb = new StringBuilder();
+            _docBuilder.MoveToHeaderFooter(HeaderFooterType.HeaderFirst);
+            sb.Append(_docBuilder.Document.GetText());
+            return sb.ToString();
+        }
     }
 }
